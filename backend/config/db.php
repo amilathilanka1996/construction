e4e5fb379e6e9be3d0e32f8f5813e148582c6b35
Amila Thilanka <amilathilanka1996@gmail.com>
2026-05-06 00:25:51 +0530

    fix: update database configuration for server environment and remove local settings

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,24 +1,10 @@
 <?php
 ##local
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=construction_manager',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8mb4',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
-
-##server
 // return [
 //     'class' => 'yii\db\Connection',
-//     'dsn' => 'mysql:host=localhost;dbname=u609401249_construction',
-//     'username' => 'u609401249_sampath',
-//     'password' => '5vTxeunTq>',
+//     'dsn' => 'mysql:host=localhost;dbname=construction_manager',
+//     'username' => 'root',
+//     'password' => '1234',
 //     'charset' => 'utf8mb4',
 
 //     // Schema cache options (for production environment)
@@ -26,4 +12,18 @@ return [
 //     //'schemaCacheDuration' => 60,
 //     //'schemaCache' => 'cache',
 // ];
+
+##server
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u609401249_construction',
+    'username' => 'u609401249_sampath',
+    'password' => '5vTxeunTq>',
+    'charset' => 'utf8mb4',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
 
